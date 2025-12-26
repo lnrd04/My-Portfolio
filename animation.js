73ea707ddb8d862314c5f95eb81f9a4e06c245de
lnrd04 <lenardnedranothe@gmail.com>
2025-12-26 17:48:16 +0800

    some changes day 4

--- animation.js
+++ animation.js
@@ -1,11 +1,22 @@
-const box = document.querySelector('.box');
+const box = document.querySelector('.about-text');
 
 window.addEventListener('scroll', () => {
   const boxTop = box.getBoundingClientRect().top;
   const windowHeight = window.innerHeight;
 
   // Trigger animation when element is 100px inside viewport
-  if (boxTop < windowHeight - 100) {
+  if (boxTop < windowHeight * 0.4) {
     box.classList.add('show');
+  }
+});
+
+const img = document.querySelector('.about-me img');
+
+window.addEventListener('scroll', () => {
+  const imgTop = img.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (imgTop < windowHeight * 0.5) {
+    img.classList.add('show');
   }
 });
